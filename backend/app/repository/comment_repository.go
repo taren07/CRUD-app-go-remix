fcc40dd43090607edb9a0f48d10fd10ce7f4e979
taren07 <taro.enchi@gmail.com>
2024-02-28 23:47:47 +0900

    update

--- backend/app/repository/comment_repository.go
+++ backend/app/repository/comment_repository.go
@@ -2,8 +2,18 @@ package repository
 
 import (
 	"app/model"
+
+	"gorm.io/gorm"
 )
 
 type ICommentRepository interface {
 	GetAllComments(comments *[]model.Comment, userId uint) error
+}
+
+type commentRepository struct {
+	db *gorm.DB
+}
+
+func NewCommentRepository(db *gorm.DB) ICommentRepository {
+	return &commentRepository{db}
 }
